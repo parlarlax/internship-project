1a7aa0bd36a6f062d68735f667a319da749e01d2
parlarlax <47736544+parlarlax@users.noreply.github.com>
2020-05-18 12:28:42 +0700

    Updates

---
+++ precache-manifest.ec979e27fb932e847fe578566ab4befc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55df66895792747b8af91baad7d5681",
+    "url": "/internship-project/index.html"
+  },
+  {
+    "revision": "5f100f62f76c4f1aa7df",
+    "url": "/internship-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb9c5f89bfc97e6c6edd",
+    "url": "/internship-project/static/js/2.d4ae6dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/internship-project/static/js/2.d4ae6dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f100f62f76c4f1aa7df",
+    "url": "/internship-project/static/js/main.13e8c10b.chunk.js"
+  },
+  {
+    "revision": "6b44abd27c3a6f8aa290",
+    "url": "/internship-project/static/js/runtime-main.883a5bf9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/internship-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
